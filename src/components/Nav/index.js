39a75a58fe5e47b64cdd6e7def0b5c18362322a2
jonathanprill <jonathantprill@gmail.com>
2022-06-16 12:30:20 -0500

    fixes nav spacing

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { Link } from 'react-router-dom';
 import Box from '@mui/material/Box';
 import SpeedDial from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
 import SpeedDialAction from '@mui/material/SpeedDialAction';
 import { WpfRetroTv, MaterialSymbolsDarkMode, MdiThemeLightDark } from "../../components/Symbols";
 
@@ -10,7 +8,7 @@ import { WpfRetroTv, MaterialSymbolsDarkMode, MdiThemeLightDark } from "../../co
 
 const Nav = () => {
   const [width, setWidth] = React.useState(window.innerWidth);
-  const breakpoint = 700;
+  const breakpoint = 1250;
   React.useEffect(() => {
     const handleResizeWindow = () => setWidth(window.innerWidth);
     // subscribe to window resize event "onComponentDidMount"
@@ -24,18 +22,12 @@ const Nav = () => {
     return (
 
       <div className="nav-container">
-
-        <Link to="/">
-          HOME
-        </Link>
-        <Link to="/retro-theme">
-          Retro
-        </Link>
-        <Box sx={{ height: 200, transform: 'translateZ(0px)', flexGrow: 1 }}>
+        <Box sx={{ height: 0, transform: 'translateZ(0px)', flexGrow: 1 }}>
           <SpeedDial
             ariaLabel="SpeedDial basic example"
-            sx={{ width: 200, position: 'absolute', bottom: -90, right: 16 }}
+            sx={{ width: 200, position: 'absolute', top: 30, left: 26 }}
             icon={<MdiThemeLightDark />}
+            direction='right'
           >
          
               <SpeedDialAction
